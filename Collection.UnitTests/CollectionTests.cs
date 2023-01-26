bbd553a9c174054762fa75064b6c24fef490f14f
Kameliya Marinova <kameliyamarinova@gmail.com>
2023-01-26 09:44:12 +0200

    add tests

--- Collection.UnitTests/CollectionTests.cs
+++ Collection.UnitTests/CollectionTests.cs
@@ -1,3 +1,5 @@
+using Collections;
+
 namespace Collection.UnitTests
 {
     public class CollectionTests
@@ -5,9 +7,42 @@ namespace Collection.UnitTests
        
 
         [Test]
-        public void Test1()
+        public void Test_Collection_EmptyConstructor() { 
+            //Arrange and Act
+            var coll = new Collection<int>();
+
+            //Assert
+            Assert.AreEqual(coll.ToString(), "[]");
+        }
+
+        [Test]
+        public void Test_Collection_ConstructorSingleItem()
         {
-            Assert.Pass();
+           var coll = new Collection<int>(5);
+           Assert.AreEqual(coll.ToString(), "[5]");
+        }
+        [Test]
+        public void Test_Collection_ConstructorMultileItems()
+        {
+            var coll = new Collection<int>(5, 6);
+            Assert.AreEqual(coll.ToString(), "[5, 6]");
+        }
+        [Test]
+        public void Test_Collection_Count()
+        {
+            var coll = new Collection<int>(5, 6);
+            Assert.AreEqual(coll.Count, 2);
+            Assert.That(coll.Capacity, Is.GreaterThanOrEqualTo(coll.Count));
+        }
+        [Test]
+        public void Test_Collection_Add()
+        {
+            var coll = new Collection<string>("Kami", "Emi");
+            coll.Add("Emo");
+            Assert.AreEqual(coll.ToString(), "[Kami, Emi, Emo]");
         }
     }
 }
+
+       
+   
