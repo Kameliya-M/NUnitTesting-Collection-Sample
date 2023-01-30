0f700c5379750e3e8f3b3b0f71a8441240150151
Kameliya Marinova <kameliyamarinova@gmail.com>
2023-01-30 12:17:40 +0200

    add DDT

--- Collection.UnitTests/CollectionTests.cs
+++ Collection.UnitTests/CollectionTests.cs
@@ -78,11 +78,20 @@ namespace Collection.UnitTests
 
             Assert.That(nums.Capacity, Is.GreaterThanOrEqualTo(nums.Count));
 
-
-
         }
-    }
+
+        [TestCase("Peter,Maria,Ivan", 0, "Peter")]
+        [TestCase("Peter,Maria,Ivan", 1, "Maria")]
+        [TestCase("Peter,Maria,Ivan", 2, "Ivan")]
+        [TestCase("Peter", 0, "Peter")]
+        public void Test_Collection_GetByValidIndex(string data, int index, string expected)
+        {
+            var coll = new Collection<string>(data.Split(","));
+            var actual = coll[index];
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        }
 }
 
        
-   
